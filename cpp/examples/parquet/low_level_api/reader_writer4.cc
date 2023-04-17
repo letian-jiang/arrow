ff23175b563b31015caf3401784e491b97539659
Letian Jiang <letian.jiang@outlook.com>
2023-04-17 14:43:45 +0800

    dingchao paruqet file
    Signed-off-by: Letian Jiang <letian.jiang@outlook.com>

--- cpp/examples/parquet/low_level_api/reader_writer4.cc
+++ cpp/examples/parquet/low_level_api/reader_writer4.cc
@@ -45,7 +45,7 @@
  * https://github.com/apache/parquet-format/blob/master/LogicalTypes.md
  **/
 
-const char PARQUET_FILENAME[] = "parquet_dingchao.parquet";
+const char PARQUET_FILENAME[] = "parquet_nested_example.parquet";
 
 int main(int argc, char** argv) {
     /**********************************************************************************
@@ -60,81 +60,79 @@ int main(int argc, char** argv) {
         std::shared_ptr<FileClass> out_file;
         PARQUET_ASSIGN_OR_THROW(out_file, FileClass::Open(PARQUET_FILENAME));
 
-        std::shared_ptr<GroupNode> schema = std::static_pointer_cast<GroupNode>(
-                GroupNode::Make("schema", Repetition::REQUIRED, {
-                        PrimitiveNode::Make("a", Repetition::REQUIRED, LogicalType::Int(64, true), Type::INT64),
-                        GroupNode::Make("b", Repetition::REQUIRED, {
-                                PrimitiveNode::Make("b1", Repetition::REQUIRED, LogicalType::Int(64, true), Type::INT64),
-                                PrimitiveNode::Make("b2", Repetition::REQUIRED, LogicalType::Int(64, true), Type::INT64),
-                        }),
-                        GroupNode::Make("c", parquet::Repetition::REQUIRED, {
-                                GroupNode::Make("key_value", Repetition::REPEATED, {
-                                        PrimitiveNode::Make("key", Repetition::REQUIRED, LogicalType::Int(64, true), Type::INT64),
-                                        PrimitiveNode::Make("value", Repetition::REQUIRED, LogicalType::Int(64, true), Type::INT64),
-                                }),
-                        }, ::parquet::LogicalType::Map()),
-                }));
+        // Setup the parquet schema
+        std::shared_ptr<GroupNode> schema = SetupNestedSchema();
 
         // Add writer properties
         parquet::WriterProperties::Builder builder;
         builder.compression(parquet::Compression::UNCOMPRESSED);
-        builder.max_row_group_length(2048);
         std::shared_ptr<parquet::WriterProperties> props = builder.build();
 
         // Create a ParquetFileWriter instance
         std::shared_ptr<parquet::ParquetFileWriter> file_writer =
                 parquet::ParquetFileWriter::Open(out_file, schema, props);
 
-        std::vector<int64_t> values(20000);
-        for (int i = 0; i < 20000; i++) {
-            values[i] = i;
-        }
+        // Append a RowGroup with a specific number of rows.
+        parquet::RowGroupWriter* rg_writer = file_writer->AppendRowGroup();
 
-        parquet::RowGroupWriter* rg_writer = file_writer->AppendBufferedRowGroup();
+        // Write the Int32 column
+        parquet::Int32Writer* int32_writer =
+                static_cast<parquet::Int32Writer*>(rg_writer->NextColumn());
 
-        for (int col = 0; col < 3; col++) {
-            auto* int64_writer = dynamic_cast<parquet::Int64Writer*>(rg_writer->column(col));
-            int64_writer->WriteBatch(8096, nullptr, nullptr, values.data());
-        }
+        std::vector<int16_t> def_levels{3, 2, 3, 3, 3};
+        std::vector<int16_t> rep_levels{0, 1, 1, 1, 1};
+        std::vector<int32_t> values{1, 2, 3, 4};
 
-        std::vector<int16_t> def_levels(20000, 1);
-        std::vector<int16_t> rep_levels(20000, 0);
-        for (int col = 0; col < 2; col++) {
-            auto* int64_writer = dynamic_cast<parquet::Int64Writer*>(rg_writer->column(col + 3));
-            int64_writer->WriteBatch(8096, def_levels.data(), rep_levels.data(), values.data());
-        }
-
-        rg_writer = file_writer->AppendBufferedRowGroup();
-
-        for (int col = 0; col < 3; col++) {
-            auto* int64_writer = dynamic_cast<parquet::Int64Writer*>(rg_writer->column(col));
-            int64_writer->WriteBatch(8096, nullptr, nullptr, values.data() + 8096);
-        }
-
-        for (int col = 0; col < 2; col++) {
-            auto* int64_writer = dynamic_cast<parquet::Int64Writer*>(rg_writer->column(col + 3));
-            int64_writer->WriteBatch(8096, def_levels.data(), rep_levels.data(), values.data() + 8096);
-        }
-
-        rg_writer = file_writer->AppendBufferedRowGroup();
-
-        for (int col = 0; col < 3; col++) {
-            auto* int64_writer = dynamic_cast<parquet::Int64Writer*>(rg_writer->column(col));
-            int64_writer->WriteBatch(20000 - 8096 * 2, nullptr, nullptr, values.data() + 8096 * 2);
-        }
-
-        for (int col = 0; col < 2; col++) {
-            auto* int64_writer = dynamic_cast<parquet::Int64Writer*>(rg_writer->column(col + 3));
-            int64_writer->WriteBatch(20000 - 8096 * 2, def_levels.data(), rep_levels.data(), values.data() + 8096 * 2);
-        }
+        uint8_t bitmap = 0b11101;
+        std::vector<int32_t> spaced_values{1, -999, 2, 3, 4};
+        int32_writer->WriteBatchSpaced(5, def_levels.data(), rep_levels.data(), &bitmap, 0, spaced_values.data());
 
         // Close the ParquetFileWriter
         file_writer->Close();
 
         // Write the bytes to file
-        DCHECK(out_file->Close().ok());
+                DCHECK(out_file->Close().ok());
     } catch (const std::exception& e) {
         std::cerr << "Parquet write error: " << e.what() << std::endl;
+        return -1;
+    }
+
+    /**********************************************************************************
+                               PARQUET READER EXAMPLE
+    **********************************************************************************/
+
+    try {
+        // Create a ParquetReader instance
+        std::unique_ptr<parquet::ParquetFileReader> parquet_reader =
+                parquet::ParquetFileReader::OpenFile(PARQUET_FILENAME, false);
+
+        // Get the File MetaData
+        std::shared_ptr<parquet::FileMetaData> file_metadata = parquet_reader->metadata();
+
+        // Get the number of RowGroups
+        int num_row_groups = file_metadata->num_row_groups();
+        assert(num_row_groups == 1);
+
+        // Get the number of Columns
+        int num_columns = file_metadata->num_columns();
+        assert(num_columns == 1);
+
+        std::shared_ptr<parquet::RowGroupReader> row_group_reader =
+                parquet_reader->RowGroup(0);
+
+        std::shared_ptr<parquet::ColumnReader> column_reader = row_group_reader->Column(0);
+        parquet::Int32Reader* int32_reader =
+                static_cast<parquet::Int32Reader*>(column_reader.get());
+
+        std::vector<int16_t> def_levels(3);
+        std::vector<int16_t> rep_levels(3);
+        std::vector<int32_t> values(2);
+        int64_t values_read;
+        int64_t levels_read = int32_reader->ReadBatch(3, def_levels.data(), rep_levels.data(), values.data(), &values_read);
+        assert(levels_read == 3);
+        assert(values_read == 2);
+    } catch (const std::exception& e) {
+        std::cerr << "Parquet read error: " << e.what() << std::endl;
         return -1;
     }
 
